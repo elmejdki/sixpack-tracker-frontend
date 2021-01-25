0bedb7bea552bddd929f4a616a92f08db8f69c6e
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-25 13:19:22 +0100

    Add a logout button to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { logout } from '../actions/auth';
 import {
   headline,
+  logoutBtn,
 } from '../stylesheet/Header.module.css';
+import logoutImage from '../assets/images/logout.png';
 
-const Header = ({ title }) => (
-  <div className={headline}>
-    {title}
-  </div>
-);
+const Header = ({ title, logout }) => {
+  const handleClick = () => {
+    logout();
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <div className={headline}>
+      {title}
+      <button
+        className={logoutBtn}
+        type="button"
+        onClick={handleClick}
+      >
+        <img
+          src={logoutImage}
+          alt="log out button"
+        />
+      </button>
+    </div>
+  );
+};
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  logout: PropTypes.func.isRequired,
 };
 
-export default Header;
+const mapDispatchToProps = {
+  logout,
+};
+
+export default connect(null, mapDispatchToProps)(Header);
