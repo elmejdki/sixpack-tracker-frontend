cf21f0d2e7a34a0e1eea4c2bb30512d510b3f1ff
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-26 06:18:40 +0100

    Add a back button to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../actions/auth';
 import {
   headline,
   logoutBtn,
+  backButton,
 } from '../stylesheet/Header.module.css';
 import logoutImage from '../assets/images/logout.png';
+import backLogo from '../assets/images/back.png';
 
-const Header = ({ title, logout }) => {
+const Header = ({ title, logout, back }) => {
+  const history = useHistory();
   const handleClick = () => {
     logout();
     localStorage.removeItem('token');
   };
 
+  const returnBack = () => {
+    history.goBack();
+  };
+
   return (
     <div className={headline}>
+      {
+        back && (
+          <button
+            className={backButton}
+            type="button"
+            onClick={returnBack}
+          >
+            <img
+              src={backLogo}
+              alt="back sign"
+            />
+          </button>
+        )
+      }
       {title}
       <button
         className={logoutBtn}
@@ -34,6 +56,11 @@ const Header = ({ title, logout }) => {
 Header.propTypes = {
   title: PropTypes.string.isRequired,
   logout: PropTypes.func.isRequired,
+  back: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  back: false,
 };
 
 const mapDispatchToProps = {
