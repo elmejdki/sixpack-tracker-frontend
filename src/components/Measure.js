da12a7c771a58a12b29b7004dd114950f5200581
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-28 05:15:36 +0100

    Remove the edit and remove actions from Measure component

--- src/components/Measure.js
+++ src/components/Measure.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { startRemoveMeasure } from '../actions/measures';
+import { Link } from 'react-router-dom';
 import youtubeLogo from '../assets/images/youtube-logo.png';
 import {
   container,
@@ -12,71 +10,45 @@ import {
   ytbLink,
   measureTitle,
   unitContainer,
-  actions,
-  edit,
-  trash,
+  link,
 } from '../stylesheet/Measure.module.css';
 
 const Measure = ({
-  id, title, image, video, unit, startRemoveMeasure,
-}) => {
-  const history = useHistory();
-
-  const handleRemove = () => {
-    startRemoveMeasure(id);
-  };
-
-  const handleEditAction = () => {
-    history.push(`/measures/edit/${id}`);
-  };
-
-  return (
-    <div className={container}>
+  id, title, image, video, unit,
+}) => (
+  <div className={container}>
+    <Link to={`/measures/edit/${id}`}>
       <img
         src={image}
         className={measureImage}
         alt="measure display"
       />
-      <div className={measureInfo}>
-        <div className={measureTitle}>
-          {title}
-        </div>
-        <div className={infoContainer}>
-          <a
-            href={video}
-            target="_blank"
-            rel="noreferrer"
-            className={ytbLink}
-          >
-            <img
-              src={youtubeLogo}
-              alt="youtube Logo"
-              className={ytbLogoImage}
-            />
-          </a>
-          <div className={unitContainer}>
-            unit: &nbsp;
-            <span>{unit}</span>
-          </div>
-        </div>
-        <div className={actions}>
-          <button
-            type="button"
-            onClick={handleEditAction}
-          >
-            <span className={edit} />
-          </button>
-          <button
-            type="button"
-            onClick={handleRemove}
-          >
-            <span className={trash} />
-          </button>
+    </Link>
+    <div className={measureInfo}>
+      <Link to={`/measures/edit/${id}`} className={`${measureTitle} ${link}`}>
+        {title}
+      </Link>
+      <div className={infoContainer}>
+        <a
+          href={video}
+          target="_blank"
+          rel="noreferrer"
+          className={ytbLink}
+        >
+          <img
+            src={youtubeLogo}
+            alt="youtube Logo"
+            className={ytbLogoImage}
+          />
+        </a>
+        <div className={unitContainer}>
+          unit: &nbsp;
+          <span>{unit}</span>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 Measure.propTypes = {
   id: PropTypes.number.isRequired,
@@ -84,11 +56,6 @@ Measure.propTypes = {
   image: PropTypes.string.isRequired,
   video: PropTypes.string.isRequired,
   unit: PropTypes.string.isRequired,
-  startRemoveMeasure: PropTypes.func.isRequired,
 };
 
-const mapDispatchToProps = {
-  startRemoveMeasure,
-};
-
-export default connect(null, mapDispatchToProps)(Measure);
+export default Measure;
