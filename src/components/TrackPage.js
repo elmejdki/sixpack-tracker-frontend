6bafc7d56c1b3bb48cecc2678d4fd2c934f68546
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-30 12:36:00 +0100

    Update the TrackPage Component to display proper scores

--- src/components/TrackPage.js
+++ src/components/TrackPage.js
@@ -1,16 +1,40 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 import Header from './Header';
+import MeasurementScore from './MeasurementScore';
 import {
   container,
 } from '../stylesheet/CommonPage.module.css';
+import { measurements } from '../simples/measurements';
+import { restructureMeasurements, getScore } from '../helpers/measurements';
 
 const TrackPage = () => (
   <div>
     <Header title="Track.it" />
     <div className={container}>
-      <h1>Track Page</h1>
+      {
+        restructureMeasurements(measurements).map(({
+          created_at: createdAt, measurements,
+        }) => (
+          <MeasurementScore
+            key={createdAt}
+            createdAt={createdAt}
+            score={getScore(measurements)}
+            measurements={measurements}
+          />
+        ))
+      }
     </div>
   </div>
 );
+
+// TrackPage.propTypes = {
+//   measurements: PropTypes.arrayOf(PropTypes.any).isRequired,
+// };
+
+// const mapStateToProps = ({ measurements }) => ({
+//   measurements,
+// });
 
 export default TrackPage;
