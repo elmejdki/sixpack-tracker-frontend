c75319ca1b7006867a99cc684ba7ff5c288372d4
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-31 01:03:12 +0100

    Add Two ProgressCircle two the progressPage

--- src/components/ProgressPage.js
+++ src/components/ProgressPage.js
@@ -1,16 +1,111 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
+import ProgressCircle from './ProgressCircle';
+import { getOnlyValueMeasurements, getScore } from '../helpers/measurements';
+import { superGoal } from '../usefull_vars';
+import {
+  progressContainer,
+  circleSize,
+  strokeSize,
+  scoreStyle,
+  scoreText,
+  // remain Score circle style
+  remainContainer,
+  remainCircleSize,
+  remainStrokeSize,
+  remainStyle,
+  remainText,
+  remainRepsText,
+} from '../stylesheet/ProgressPage.module.css';
 import {
   container,
+  removeWhiteSpaceOnTop,
 } from '../stylesheet/CommonPage.module.css';
 
-const ProgressPage = () => (
-  <div>
-    <Header title="Progress report" />
-    <div className={container}>
-      <h1>Progress Page</h1>
-    </div>
-  </div>
-);
+const ProgressPage = ({ onlyValyeMeasurements, measuresSize }) => {
+  const score = getScore(onlyValyeMeasurements);
+  const longGoal = superGoal(measuresSize);
 
-export default ProgressPage;
+  return (
+    <div>
+      <Header title="Progress report" />
+      <div className={container}>
+        <div className={removeWhiteSpaceOnTop} />
+        <div
+          className={progressContainer}
+        >
+          <ProgressCircle
+            size="70"
+            dashoffset={440}
+            circleSize={circleSize}
+            strokeSize={strokeSize}
+            score={score}
+            goal={longGoal}
+            green
+          >
+            <div
+              className={scoreStyle}
+            >
+              <div>
+                You did
+              </div>
+              <div
+                className={scoreText}
+              >
+                {score}
+              </div>
+              <div>
+                reps
+              </div>
+            </div>
+          </ProgressCircle>
+        </div>
+        <div
+          className={remainContainer}
+        >
+          <ProgressCircle
+            size="40"
+            dashoffset={252}
+            circleSize={remainCircleSize}
+            strokeSize={remainStrokeSize}
+            score={longGoal - score}
+            goal={longGoal}
+            red
+            flip
+          >
+            <div
+              className={remainStyle}
+            >
+              <div
+                className={remainText}
+              >
+                {longGoal - score}
+              </div>
+            </div>
+          </ProgressCircle>
+          <div
+            className={remainRepsText}
+          >
+            More Reps to Do
+          </div>
+        </div>
+        <h1>Progress Page</h1>
+      </div>
+    </div>
+  );
+};
+
+ProgressPage.propTypes = {
+  onlyValyeMeasurements: PropTypes.arrayOf(PropTypes.object).isRequired,
+  measuresSize: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = ({ measurements, measures }) => ({
+  measurements,
+  onlyValyeMeasurements: getOnlyValueMeasurements(measurements),
+  measuresSize: measures.length,
+});
+
+export default connect(mapStateToProps)(ProgressPage);
