b826f77724f3f33055df84d9cce2df02429abe8a
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-02-01 12:45:25 +0100

    Complete MorePage component

--- src/components/MorePage.js
+++ src/components/MorePage.js
@@ -1,16 +1,95 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
+import target from '../assets/images/target.png';
+import email from '../assets/images/email.png';
+import man from '../assets/images/man.png';
+import settings from '../assets/images/settings.png';
+import help from '../assets/images/help.png';
 import {
   container,
 } from '../stylesheet/CommonPage.module.css';
+import {
+  user,
+  info,
+  link,
+} from '../stylesheet/MorePage.module.css';
 
-const MorePage = () => (
+const MorePage = ({ userData }) => (
   <div>
     <Header title="More" />
     <div className={container}>
-      <h1>More Page</h1>
+      <div
+        className={user}
+      >
+        <img
+          alt="user face hahaha"
+          src={`http://localhost:3000${userData.avatar}`}
+        />
+        <div
+          className={info}
+        >
+          <div>{userData.username}</div>
+          <div>{userData.email}</div>
+        </div>
+      </div>
+      <div
+        className={link}
+      >
+        <img
+          alt="target"
+          src={target}
+        />
+        <div>Your goal</div>
+      </div>
+      <div
+        className={link}
+      >
+        <img
+          alt="target"
+          src={email}
+        />
+        <div>Mailing listl</div>
+      </div>
+      <div
+        className={link}
+      >
+        <img
+          alt="target"
+          src={man}
+        />
+        <div>Your profile</div>
+      </div>
+      <div
+        className={link}
+      >
+        <img
+          alt="target"
+          src={settings}
+        />
+        <div>Settings</div>
+      </div>
+      <div
+        className={link}
+      >
+        <img
+          alt="target"
+          src={help}
+        />
+        <div>Help</div>
+      </div>
     </div>
   </div>
 );
 
-export default MorePage;
+MorePage.propTypes = {
+  userData: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  ).isRequired,
+};
+
+const mapStateToProps = ({ userData }) => ({
+  userData,
+});
+
+export default connect(mapStateToProps)(MorePage);
