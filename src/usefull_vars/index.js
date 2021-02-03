20ab3c33a74feadab1b0beeae9976acd4a33c969
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-02-03 15:29:43 +0100

    Return back to the correct host

--- src/usefull_vars/index.js
+++ src/usefull_vars/index.js
@@ -1,7 +1,7 @@
 export const repGoal = 35;
 
 export const superGoal = measuresSize => repGoal * 60 * measuresSize;
-// https://six-pack-tracker-api.herokuapp.com
-export const host = 'http://localhost:3000';
+
+export const host = 'https://six-pack-tracker-api.herokuapp.com';
 
 export const removeDelay = 5000;
